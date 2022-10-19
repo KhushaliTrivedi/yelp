e5a132095c35478c5ec16e557294aa505e930ea7
Khushali Trivedi <khushalitrivedi47@gmail.com>
2022-10-19 15:07:48 +0530

    add start scripts

--- app.js
+++ app.js
@@ -153,6 +153,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 })
 
-app.listen('3000', () => {
-    console.log('SERVING ON PORT 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`SERVING ON PORT ${port}`);
 })
